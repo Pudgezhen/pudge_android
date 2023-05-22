053fc727b4539c54d96dd7a834c7212086645b3b
pudge <1184321378@qq.com>
2023-05-22 16:57:00 +0800

    测试button点击事件

--- app/src/main/java/com/example/pudge/MainActivity.java
+++ app/src/main/java/com/example/pudge/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.pudge;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.test);
+        button.setOnClickListener((View v)->
+                Log.i("test","测试")
+        );
     }
 }
